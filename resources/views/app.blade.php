d9048280c821d97b6894eb06ba4d2010d80e42a7
Wayne Bishop <bishopwb66@gmail.com>
2016-12-04 15:24:48 +1300

    added contact and about and pagescontroller

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	
+	<meta charset="UTF-8">
+	<title>Document Tilte</title>
+</head>
+
+<body>
+	<div class="container">
+
+		@yield('content')
+
+	</div>
+
+	@yield('footer')
+
+
+</body>
+
+</html>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+
+	
+
+	<h1>About Me {{ $first }} {{ $last }} </h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita repellendus sapiente eaque ad facere distinctio cupiditate quas mollitia recusandae incidunt neque iste impedit illum doloremque asperiores sint aut nostrum, delectus! </p>	
+
+@stop
