98000bbe40a0a3217473cdc7516e77a42700f59b
Wayne Bishop <bishopwb66@gmail.com>
2016-12-07 09:36:52 +1300

    added routes for auth but commented out

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,18 @@
 
 Route::get('/', 'HomeController@index');
 
+
+// Authentication routes
+// Route::get('auth/login', 'Auth\AuthController@getLogin');
+// Route::get('auth/login', 'Auth\AuthController@postLogin');
+// Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+
+// Registration routes
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+
 Route::get('about', 'PagesController@about');
 
 Route::get('contact', 'PagesController@contact');
