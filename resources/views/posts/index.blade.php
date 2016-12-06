f91f261307d1eed8640f4d801ee0687177a2ab86
Wayne Bishop <bishopwb66@gmail.com>
2016-12-07 11:39:45 +1300

    added content to blog index and show pages

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,20 +6,293 @@
 
 	<h1>Posts</h1>
 
+	<!-- DYNAMIC CONTENT -->
+
 	@foreach ($posts as $post)
 		<article>
-			<h2>
-				<a href="/posts/{{ $post->id }} ">{{ $post->title }}</a>
-			</h2>	
 
-			<p>{{ $post->intro }}</p>
+			<!-- Post Row 1 -->
+			<!-- Image for Post 1 -->
+			<div class="row post-row">
+			    <div class="col-sm-12 col-md-7">            
+			         <a href="/posts/{{ $post->id }}">
+			            <img class="img-responsive img-rounded" src="img/andyBatsman_Medium.jpg" alt="">
+			        </a>
+			    </div>    
+			    <!-- Text for MD and LG Screens -->
+			    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs"> 
+			        <h2 class="text-left"><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+			        <p class="text-left"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+			        <p>Created: <span>{{ $post->created_at }}</span> Updated: <span>{{ $post->updated_at }}</span></small></p>
+			        <p class="lead text-left">{{ $post->intro }}</p>
+			        <a href="/posts/{{ $post->id }}" class="btn btn-info pull-left">Read More</a>         
+			    </div>
 
-			<div class="body">{{ $post->body }}</div>
+			    <!-- Text for SM and XS Screens -->
+			    <div class="col-sm-12 col-md-5 hidden-md hidden-lg"> 
+			        <h2 class="text-center">{{ $post->title }}</h2>
+			        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+			        <p>Created: <span>{{ $post->created_at }} </span>Updated: <span>{{ $post->updated_at }} </span></small></p>
+			        <p>{{ $post->intro }}</p>
+			        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+			    </div>
+
+			</div> <!-- Close Post Row 1 -->
+
 
 		</article>
 
 	@endforeach
 
+<!-- OLD STATIC CONTENT -->
+	
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-sm-12">
+        <h1 class="page-header gallery-header">Good Chat</h1>
+    </div>
+</div>
+<!-- /.row -->
+
+<div class="row">
+
+	<!-- Tags Well -->
+    
+    <div class="col-sm-7">
+        <div class="well search-well">
+            <a href="#" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span> Photography</a>
+
+            <a href="#" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a>
+     
+            <a href="#" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-grain" aria-hidden="true"></span> Other</a>
+        </div>        
+    </div>
+
+	<!-- Search Well -->
+    <div class="col-sm-5">
+        <div class="well">
+                               
+            <form action="" method="post">
+                <div class="input-group">
+                    <input type="search" name="search" class="form-control" placeholder="Search title and article only.">
+                    <span class="input-group-btn">
+                    <button type="submit" class="btn btn-sm btn-default">
+                        <a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></a>
+                    </button>
+                    </span>
+                </div>
+                <!-- /.input-group -->
+            </form>
+
+        </div>        
+    </div>
+</div>
+
+<hr>
+
+<!-- Blog Roll -->
+
+<!-- Post Row 1 -->
+<!-- Image for Post 1 -->
+<div class="row post-row">
+    <div class="col-sm-12 col-md-7">            
+         <a href="">
+            <img class="img-responsive img-rounded" src="img/andyBatsman_Medium.jpg" alt="">
+        </a>
+    </div>    
+    <!-- Text for MD and LG Screens -->
+    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs"> 
+        <h2 class="text-left">Medium: A good day with the bat.</h2>
+        <p class="text-left"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p>Created: <span>16 Nov 2016 </span> Updated: <span>20 Nov 2016 </span></small></p>
+        <p class="lead text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit... </p>
+        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+    </div>
+
+    <!-- Text for SM and XS Screens -->
+    <div class="col-sm-12 col-md-5 hidden-md hidden-lg"> 
+        <h2 class="text-center">Small: A good day with the bat.</h2>
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p>Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit... </p>
+        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+    </div>
+
+</div> <!-- Close Post Row 1 --> 
+
+<!-- Post Row 2 -->    
+<div class="row post-row">
+    <!-- Text for MD and LG Screens -->            
+    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs">
+        <h2 class="text-right">Medium: Left nothing on the table.</h2>
+        <p class="text-right"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span> Photography</a><small> Author: <span>Shaun Bishop</span></p>
+        <p class="text-right">Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p class="lead text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit...</p>
+        <a href="" class="btn btn-info pull-right">Read More</a>
+    </div>
+    
+    <!-- Image for Post -->
+    <div class="col-sm-12 col-md-7">            
+        <a href="blog_post.html">
+            <img class="img-responsive img-rounded" src="img/bowler_Medium.jpg" alt="">
+        </a>               
+    </div>
+
+    <!-- Text for SM and XS Screens -->    
+    <div class="col-sm-12 col-md-5 hidden-md hidden-lg">
+        <h2 class="text-center">Small: Left nothing on the table.</h2>
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-camera" aria-hidden="true"></span> Photography</a><small> Author: <span>Shaun Bishop</span></p>
+        <p> Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit...</p> 
+        <a href="" class="btn btn-info">Read More</a>
+    </div>
+
+</div> <!-- Close Post Row 2 -->
+        
+<!-- Post Row 3 -->
+<!-- Image for Post 3 -->
+<div class="row post-row">
+    <div class="col-sm-12 col-md-7">            
+         <a href="">
+            <img class="img-responsive img-rounded" src="img/andyBatsman_Medium.jpg" alt="">
+        </a>
+    </div>    
+    <!-- Text for MD and LG Screens -->
+    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs"> 
+        <h2 class="text-left">Medium: Solid 50 on tough day.</h2>
+        <p class="text-left"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p>Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p class="lead text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit... </p>
+        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+    </div>
+
+    <!-- Text for SM and XS Screens -->
+    <div class="col-sm-12 col-md-5 hidden-md hidden-lg"> 
+        <h2 class="text-center">Small: Solid 50 on tough day.</h2>
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p>Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit... </p>
+        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+    </div>
+
+</div> <!-- Close Post Row 3 --> 
+
+<!-- Post Row 4 -->    
+<div class="row post-row">
+    <!-- Text for MD and LG Screens -->            
+    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs">
+        <h2 class="text-right">Medium: The old trooper fires at Whitby.</h2>
+        <p class="text-right"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p class="text-right">Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p class="lead text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit...</p>
+        <a href="" class="btn btn-info pull-right">Read More</a>
+    </div>
+    
+    <!-- Image for Post -->
+    <div class="col-sm-12 col-md-7">            
+        <a href="blog_post.html">
+            <img class="img-responsive img-rounded" src="img/bowler_Medium.jpg" alt="">
+        </a>               
+    </div>
+
+    <!-- Text for SM and XS Screens -->    
+    <div class="col-sm-12 col-md-5 hidden-md hidden-lg">
+        <h2 class="text-center">Small: The old trooper fires at Whitby.</h2>
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p> Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit...</p>
+        <a href="" class="btn btn-info">Read More</a>
+    </div>
+
+</div> <!-- Close Post Row 4 -->
+
+<!-- Post Row 5 -->
+<!-- Image for Post 5 -->
+<div class="row post-row">
+    <div class="col-sm-12 col-md-7">            
+         <a href="">
+            <img class="img-responsive img-rounded" src="img/andyBatsman_Medium.jpg" alt="">
+        </a>
+    </div>    
+    <!-- Text for MD and LG Screens -->
+    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs"> 
+        <h2 class="text-left">Medium: A good day with the bat.</h2>
+        <p class="text-left"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p>Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p class="lead text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit... </p>
+        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+    </div>
+
+    <!-- Text for SM and XS Screens -->
+    <div class="col-sm-12 col-md-5 hidden-md hidden-lg"> 
+        <h2 class="text-center">Small: A good day with the bat.</h2>
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p>Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit... </p>
+        <a href="blog_post.html" class="btn btn-info pull-left">Read More</a>         
+    </div>
+
+</div> <!-- Close Post Row 5 --> 
+
+<!-- Post Row 6 -->    
+<div class="row post-row">
+    <!-- Text for MD and LG Screens -->            
+    <div class="col-sm-12 col-md-5 hidden-sm hidden-xs">
+        <h2 class="text-right">Medium: Left nothing on the table.</h2>
+        <p class="text-right"><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p class="text-right">Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p class="lead text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit...</p>
+        <a href="" class="btn btn-info pull-right">Read More</a>
+    </div>
+    
+    <!-- Image for Post -->
+    <div class="col-sm-12 col-md-7">            
+        <a href="blog_post.html">
+            <img class="img-responsive img-rounded" src="img/bowler_Medium.jpg" alt="">
+        </a>               
+    </div>
+
+    <!-- Text for SM and XS Screens -->    
+    <div class="col-sm-12 col-md-5 hidden-md hidden-lg">
+        <h2 class="text-center">Small: Left nothing on the table.</h2>
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Sport</a><small> Author: <span>Shaun Bishop</span></p>
+        <p> Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt vitae repellat non cum atque dolores consequuntur dolore consectetur adipisicing elit...</p>
+        <a href="" class="btn btn-info">Read More</a>
+    </div>
+
+</div> <!-- Close Post Row 6 -->
+
+
+<!-- Pagination -->
+<div class="row text-center">
+    <div class="col-lg-12">
+        <ul class="pagination">
+            <li>
+                <a href="#">&laquo;</a>
+            </li>
+            <li class="active">
+                <a href="#">1</a>
+            </li>
+            <li>
+                <a href="#">2</a>
+            </li>
+            <li>
+                <a href="#">3</a>
+            </li>
+            <li>
+                <a href="#">4</a>
+            </li>
+            <li>
+                <a href="#">5</a>
+            </li>
+            <li>
+                <a href="#">&raquo;</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<!-- /.row -->
 
 @stop
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,118 @@
 
 @section('content')
 
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-sm-12">
+        <h1 class="page-header gallery-header">Good Chat</h1>
+    </div>
+</div>
+<!-- /.row -->
+
+<!-- Blog Post -->
+
+<!-- Post Row 1 -->
+<!-- Image for Post -->
+<div class="row post-row">
+    <div class="col-sm-12 col-md-8 col-md-offset-2">            
+         <a href="/galleries">
+            <img class="img-responsive img-rounded" src="/img/andyBatsman_Medium.jpg" alt="">
+        </a>
+
+        <h2 class="text-center">{{ $post->title }}<a href="gallery_slider.html" class="btn btn-xs btn-info pull-right"><span class="glyphicon glyphicon-film" aria-hidden="true"></span> Gallery</a></h2>
+        
+        <p><a href="#" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> SPORT</a><small> Author: <span>Shaun Bishop </span></p>
+        
+        <p>Created: <span>16 Nov 2016 </span>Updated: <span>20 Nov 2016 </span></small></p>
+        
+        <p class="lead">{{ $post->intro }}</p>
+
+        <p>{{ $post->body }}</p>
+
+        <!-- Comments Form -->
+        <div class="well">
+            <h4>Leave a Comment:</h4>
+            <form role="form">
+                <div class="form-group">
+                    <textarea class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+
+        <hr>
+
+        <!-- Posted Comments -->
+
+        <!-- Comment 1 -->
+        <div class="media">
+            <a class="pull-left" href="#">
+                <img class="media-object" src="http://placehold.it/64x64" alt="">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Greg Chappell
+                    <small>October 2, 2016 at 9:30 AM</small>
+                </h4>
+                Fair dinkum this is the best post I have ever read. Can't wait for the next game.
+            </div>
+        </div>
+
+        <!-- Comment 2 -->
+        <div class="media">
+            <a class="pull-left" href="#">
+                <img class="media-object" src="http://placehold.it/64x64" alt="">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Kane Williamson
+                    <small>October 2, 2016 at 9:30 AM</small>
+                </h4>
+                Good knock mate. Keep it up.
+            </div>
+        </div>
+
+        <!-- Comment 3 -->
+        <div class="media">
+            <a class="pull-left" href="#">
+                <img class="media-object" src="http://placehold.it/64x64" alt="">
+            </a>
+            <div class="media-body">
+                <h4 class="media-heading">Guppy
+                    <small>October 2, 2016 at 9:30 AM</small>
+                </h4>
+                Well done. Good to see the old fella has still got it.
+            </div>
+        </div>
+
+        <hr> 
+
+        <a class="btn btn-primary" href="#">More Comments <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></a>
+        
+        <hr>
+
+        <a href="blog_roll.html" class="btn btn-info pull-left"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Back</a>
+
+    </div>
+
+</div>    
+
+<hr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	<h1>{{ $post->title }}</h1>
 
 	<article>
